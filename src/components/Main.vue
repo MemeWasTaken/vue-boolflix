b974bd9e3a346d66f7d5b8228cc9278bf31af2fb
MemeWasTaken <memelazzari02@gmail.com>
2022-01-19 15:41:44 +0100

    help

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,8 +4,29 @@
 </template>
 
 <script>
+    import axios from 'axios';
+
     export default {
         name: "Main",
+        data() {
+            return {
+                tryout: "all",
+                movies: null,
+            };
+        },
+        methods: {
+            getMovies() {
+                axios
+                    .get(`https://api.themoviedb.org/3/search/movie?api_key=9da47a4b22fa9d371b93ff9409a34189&query=${this.tryout}`)
+                    .then((result) => {
+                        console.log(result.data.results);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            },
+        },
+        
     }
 </script>
 
